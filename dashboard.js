5dccf0e749d63558ba806880eedcd6570816f41f
chandini <chandini@belfricsbt.com>
2018-12-28 15:19:08 +0530

    completed loginpage

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,23 @@
+var role=localStorage.getItem("roleId");
+
+function model(){
+    console.log(role);
+    if((role==="superuser")||(role==="new user")){
+     var list = document.getElementById('heading_list');
+    list.childNodes[5].remove();
+    list.childNodes[1].remove();
+    }
+    if(role==="issuer"){
+        var list = document.getElementById('heading_list');
+        list.childNodes[5].remove();
+        list.childNodes[3].remove();
+    }  
+    if(role==="authorizer"){
+        var list = document.getElementById('heading_list');
+        list.childNodes[3].remove();
+        list.childNodes[1].remove();
+    }
+ }
 var belriumtoken="f00eef6b-f8c6-4c47-ae1a-647363b8ad831545379936918";
 async function issueDashboard(){
 
