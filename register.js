6dd2ac5c22d36424640671af81592fb3f18a9a2d
chandini <chandini@belfricsbt.com>
2018-12-29 11:04:49 +0530

    register

--- register.js
+++ register.js
@@ -1,16 +1,17 @@
-var lastName,name,email,password,type,countryCode,countryId;
+var lastName,name,email,password,type,countrycode,countryid;
+var countryData;
+const count_countries=253;
 $(document).ready(function(){
-var email = localStorage.getItem("Email");  
-document.getElementById("email").setAttribute("value",email);
+// var email = localStorage.getItem("Email");  
+// document.getElementById("email").setAttribute("value",email);
     $("#register").click(async function(){
-        if(signupValidation()){
-        countryCode=escapeInput($("#countryCode").val());
-        countryId=escapeInput($("#countryId").val());
+        //if(signupValidation()){
+            console.log("hai");
         email=escapeInput($("#email").val());
         lastName=escapeInput($("#name").val());
         name=escapeInput($("#name").val());
         password=escapeInput($("#password").val());
-        type="merchant";
+        type=escapeInput($("#user-type").val());
         const signupResponse= await checkSignup();
         if(signupResponse["isSuccess"]==true){
             console.log(signupResponse);
@@ -20,7 +21,7 @@ document.getElementById("email").setAttribute("value",email);
         else{
             $("#message").html("signup failed");
         }
-    }
+   // }
 });
 });
 async function checkSignup()
@@ -29,8 +30,8 @@ async function checkSignup()
         try{
                result=await $.ajax({
                    type: 'post',
-                   url: 'http://54.254.174.74:8080/api/v1/signup',
-                   data: '{"countryCode":"' + countryCode +'","countryId":"' + countryId +'","email":"' + email +'","lastName":"' + lastName +'","name":"' + name +'","password":"' + password +'","type":"' + type +'"}',
+                   url: HOST_URL+"/"+SDAPP_ID+"/"+"user/signup",
+                   data: '{"countryCode":"' + countrycode +'","countryId":"' + countryid +'","email":"' + email +'","lastName":"' + lastName +'","name":"' + name +'","password":"' + password +'","type":"' + type +'"}',
                    contentType: 'application/json;charset=UTF-8',
                    dataType: 'json'
                });
@@ -57,3 +58,30 @@ function escapeInput(input) {
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;');
     }
+
+function getCountries()
+{
+    $.get("http://54.254.174.74:8080/api/v1/countries",function(data){
+        // console.log(data);
+        countryData = data;
+        var x = document.getElementById("country_list");
+        for(var i=0 ;i<count_countries;i++){
+            var option = document.createElement("option");
+            option.text = countryData['data'][i]['countryName'];
+            x.add(option);
+        }
+    });
+}
+
+function countryValidator(){
+    var e = document.getElementById("country_list");
+    // console.log(e);
+    var strUser = e.options[e.selectedIndex].text;
+    // console.log(e.options[e.selectedIndex]);
+    var i=e.selectedIndex;
+    console.log(strUser);
+    console.log(countryData['data'][i]['countryCode']);
+    console.log(countryData['data'][i]['countryID']);
+    countrycode=countryData['data'][i]['countryCode'];
+    countryid=countryData['data'][i]['countryID']
+}
